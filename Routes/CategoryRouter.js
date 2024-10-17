b00691ee8c32d8c30fe233e91f89b5eb2b1c2482
Sawdha Mufeedh <sawdhamufeedh@gmail.com>
2024-10-17 11:58:40 +0530

    chnages made in the category routes and controller

--- Routes/CategoryRouter.js
+++ Routes/CategoryRouter.js
@@ -1,9 +1,24 @@
 import express from 'express'
-import { Createcategory } from '../Controller/CategoryController.js'
+import { Createcategory, deleteCategory, getCategoryByName } from '../Controller/CategoryController.js'
 
 const CategoryRouter=express.Router();
 
 CategoryRouter.post("/", Createcategory)
+
+CategoryRouter.delete("/:name",deleteCategory)
+
+CategoryRouter.get("/searchbyPrice", (req,res)=>{
+    res.json({
+
+        message:"Searchbyprice"
+
+    })
+})
+
+CategoryRouter.get("/name" ,getCategoryByName)
+
+CategoryRouter.get("/",getCategory);
+
 
 export default CategoryRouter;
 
